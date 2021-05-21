33b48ef924cfd8ef86207e3620aba6a3d2277045
divyanshu45 <mdivyanshu996@gmail.com>
2021-05-21 21:40:57 +0530

    Implement Interface

--- app/src/main/java/com/example/bhagvatgeetaapp/AllChapters.kt
+++ app/src/main/java/com/example/bhagvatgeetaapp/AllChapters.kt
@@ -1,8 +1,11 @@
 package com.example.bhagvatgeetaapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.lifecycle.Observer
@@ -14,6 +17,8 @@ import com.example.bhagvatgeetaapp.models.VersesItem
 import com.example.bhagvatgeetaapp.ui.GeetaViewModel
 import com.example.bhagvatgeetaapp.ui.ViewModelFactory
 import com.example.bhagvatgeetaapp.utils.Status
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_all_chapters.*
@@ -25,6 +30,7 @@ class AllChapters : AppCompatActivity() {
     private lateinit var viewModel: GeetaViewModel
     private lateinit var adapter: AllChaptersAdapter
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_chapters)
@@ -32,8 +38,21 @@ class AllChapters : AppCompatActivity() {
         viewModel = ViewModelProviders.of(this, ViewModelFactory()).get(GeetaViewModel::class.java)
 
         all_chapters_recycler_view.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        adapter = AllChaptersAdapter(applicationContext,provideChapters())
+        adapter = AllChaptersAdapter(provideChapters())
         all_chapters_recycler_view.adapter = adapter
+
+        adapter.setOnItemClickListener {
+
+            val bottomSheetDialog = BottomSheetDialog(this)
+            bottomSheetDialog.setContentView(R.layout.chapter_info_extra)
+            bottomSheetDialog.show()
+
+//            val intent = Intent(applicationContext, AllVerse::class.java)
+//            intent.putExtra("Number", it.chapter_number)
+//            intent.putExtra("Name", it.name)
+//            Log.d("TAG", it.name)
+//            startActivity(intent)
+        }
 
     }
 
--- app/src/main/java/com/example/bhagvatgeetaapp/adapters/AllChaptersAdapter.kt
+++ app/src/main/java/com/example/bhagvatgeetaapp/adapters/AllChaptersAdapter.kt
@@ -1,18 +1,14 @@
 package com.example.bhagvatgeetaapp.adapters
 
-import android.content.Context
-import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.bhagvatgeetaapp.AllVerse
 import com.example.bhagvatgeetaapp.R
 import com.example.bhagvatgeetaapp.models.ChaptersItem
 import kotlinx.android.synthetic.main.item_chapter_list.view.*
 
-class AllChaptersAdapter(val context: Context, val chapterList: ArrayList<ChaptersItem>) : RecyclerView.Adapter<AllChaptersAdapter.ViewHolder>() {
+class AllChaptersAdapter(private val chapterList: ArrayList<ChaptersItem>) : RecyclerView.Adapter<AllChaptersAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AllChaptersAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.item_chapter_list, parent, false)
@@ -22,11 +18,7 @@ class AllChaptersAdapter(val context: Context, val chapterList: ArrayList<Chapte
     override fun onBindViewHolder(holder: AllChaptersAdapter.ViewHolder, position: Int) {
         holder.bindItems(chapterList[position])
         holder.itemView.setOnClickListener {
-            val intent = Intent(context, AllVerse::class.java)
-            intent.putExtra("Number", position+1)
-            intent.putExtra("Name", chapterList[position].name)
-            Log.d("TAG", chapterList[position].name)
-            context.startActivity(intent)
+            onItemClickListener?.let { it(chapterList[position]) }
         }
     }
 
@@ -35,9 +27,13 @@ class AllChaptersAdapter(val context: Context, val chapterList: ArrayList<Chapte
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
         fun bindItems(chapter: ChaptersItem) {
-            itemView.chapter_number.text = "Chapter - ${chapter.chapter_number.toString()}"
+            itemView.chapter_number.text = "Chapter - ${chapter.chapter_number}"
         }
+    }
+
+    private var onItemClickListener: ((ChaptersItem) -> Unit)? = null
+    fun setOnItemClickListener(listener: (ChaptersItem) -> Unit) {
+        onItemClickListener = listener
     }
 }
